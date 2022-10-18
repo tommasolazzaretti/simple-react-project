76465dac9a3a7fc4afc0b6e071fcf372fe933b1f
tommaso.lazzaretti <tommaso.lazzaretti@gmail.com>
2022-10-18 16:05:28 +0200

    environment config

--- next.config.js
+++ next.config.js
@@ -5,3 +5,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+};
--- src/features/post/postsAction.ts
+++ src/features/post/postsAction.ts
@@ -2,7 +2,7 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const GET_POSTS_LIST = createAsyncThunk('post/GET_POSTS_LIST', async (params: any) => {
-    const response = await axios.post("http://127.0.0.1:8080/getPost", {
+    const response = await axios.post(`${process.env.API_URL}/getPost`, {
         limit: params.pagination.limit,
         page: params.pagination.page,
     });
@@ -10,21 +10,21 @@ export const GET_POSTS_LIST = createAsyncThunk('post/GET_POSTS_LIST', async (par
 });
 
 export const GET_POST_DETAILS = createAsyncThunk('post/GET_POST_DETAILS', async (params) => {
-    const response = await axios.post("http://127.0.0.1:8080/getPost");
+    const response = await axios.post(`${process.env.API_URL}/getPost`);
     return response.data;
 });
 
 export const CREATE_POST = createAsyncThunk('post/CREATE_POST', async (params) => {
-    const response = await axios.post("http://127.0.0.1:8080/createPost", params);
+    const response = await axios.post(`${process.env.API_URL}/createPost`, params);
     return response.data;
 });
 
 export const UPDATE_POST = createAsyncThunk('post/UPDATE_POST', async (params) => {
-    const response = await axios.post("http://127.0.0.1:8080/updatePost", params);
+    const response = await axios.post(`${process.env.API_URL}/updatePost`, params);
     return response.data;
 });
 
 export const DELETE_POST = createAsyncThunk('post/DELETE_POST', async (params) => {
-    const response = await axios.post("http://127.0.0.1:8080/deletePost", params);
+    const response = await axios.post(`${process.env.API_URL}/deletePost`, params);
     return response.data;
 });
